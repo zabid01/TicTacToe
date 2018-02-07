5290eb84c8b48c1d13918a68b55ced7b846f1115
zabid01 <zabid.antor@gmail.com>
2018-02-07 09:30:09 +0600

    class work

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,14 @@
 //JS code goes here
+
+
+var turn = false;
+// var
+function play(event){
+  if (!event.target.innerText)
+  {
+    event.target.innerText = turn ? "O" : "1";
+    turn= !turn;
+
+  }
+
+}
